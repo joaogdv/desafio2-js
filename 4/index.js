b158766f43ad727f60e80eb5a550ee9a800f9e95
João Guilherme Domingues <142355184+joaogdv@users.noreply.github.com>
2023-09-19 14:41:56 -0300

    Add files via upload

---
+++ 4/index.js
@@ -0,0 +1,7 @@
+// Defina um array de exemplo
+var meuArray = [1, 2, 3, 4, 5];
+
+// Crie um loop for que começa do último elemento até o primeiro
+for (var i = meuArray.length - 1; i >= 0; i--) {
+  console.log(meuArray[i]);
+}
